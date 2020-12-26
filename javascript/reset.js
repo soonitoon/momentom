955e26fa61cb577579993ab980c9567b2d17680c
soonitoon <soonitoon@gmail.com>
2020-12-26 14:51:28 +0900

    오류 수정

--- javascript/reset.js
+++ javascript/reset.js
@@ -3,7 +3,7 @@ const resetBtn = document.querySelector(".js-reset");
 function reset() {
     resetBtn.addEventListener("click", () => {
         localStorage.clear();
-        location.href="/index"
+        location.href="./"
     })
 }
 
